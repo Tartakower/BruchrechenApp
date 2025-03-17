08dc4f35e5af877c98dfe72bdacbb3f321576c21
Polu Tartakower <phish@quantentunnel.de>
2025-03-17 19:30:56 +0100

    Bruch-Klasse

---
+++ bruchrechenapp/src/model/bruch.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Bruch():
+
+    zaehler: int
+    nenner: int
+
+    def __init__(self, zaehler: int, nenner: int) -> None:
+        if nenner == 0:
+            raise ValueError("nenner == 0")
+        faktor = 1 if nenner > 0 else -1
+        object.__setattr__(self, "zaehler", zaehler * faktor)
+        object.__setattr__(self, "nenner", nenner * faktor)
+
+    def kuerze(self) -> Bruch:
+        teiler = ggT(self.zaehler, self.nenner)
+        return Bruch(self.zaehler // teiler, self.nenner // teiler)
+
+    def inverses(self) -> Bruch:
+        return Bruch(-self.zaehler,self.nenner)
+    
+    def kehrwert(self) -> Bruch:
+        return Bruch(self.nenner,self.zaehler)
+    
+    def addiere(self, b: Bruch) -> Bruch:
+        return Bruch(self.zaehler * b.nenner + self.nenner * b.zaehler, self.nenner * b.nenner).kuerze()
+    
+    def subtrahiere(self, b: Bruch) -> Bruch:
+        return self.addiere(b.inverses())
+    
+    def multipliziere(self, b: Bruch) -> Bruch:
+        return Bruch(self.zaehler * b.zaehler, self.nenner * b.nenner).kuerze()
+    
+    def dividiere(self, b: Bruch) -> Bruch:
+        return self.multipliziere(b.kehrwert())
+
+def ggT(a: int, b: int) -> int:
+    if b < 0: return ggT(a,-b)
+    while b != 0:
+        h = a % b
+        a = b
+        b = h
+    return a
---
+++ bruchrechenapp/src/test/test_bruch.py
@@ -0,0 +1,45 @@
+import pytest
+from bruchrechenapp.src.model.bruch import Bruch, ggT
+
+
+def test_constructor() -> None:
+    b = Bruch(2,4)
+    assert 2 == b.zaehler
+    assert 4 == b.nenner
+
+    with pytest.raises(ValueError) as info:
+        Bruch(1,0)
+    assert "nenner == 0" in str(info.value)
+
+    b = Bruch(1,-2)
+    assert -1 == b.zaehler
+    assert 2 == b.nenner
+
+def test_equals() -> None:
+    assert Bruch(1,2) == Bruch(1,2)
+    assert Bruch(1,2) != Bruch(2,4)
+
+def test_ggT() -> None:
+    assert 2 == ggT(2,4)
+    assert 13 == ggT(65, 143)
+    assert 1 == ggT(7,31)
+    assert 2 == ggT(-2,4)
+    assert 2 == ggT(2,-4)
+
+def test_kuerze() -> None:
+    assert Bruch(1,2) == Bruch(6,12).kuerze()
+    assert Bruch(-2,3) == Bruch(-24,36).kuerze()
+
+def test_addiere() -> None:
+    assert Bruch(5,6) == Bruch(1,2).addiere(Bruch(1,3))
+    assert Bruch(3,4) == Bruch(1,2).addiere(Bruch(1,4))
+
+def test_subtrahiere() -> None:
+    assert Bruch(-1,6) == Bruch(1,3).subtrahiere(Bruch(1,2))
+    assert Bruch(1,4) == Bruch(1,2).subtrahiere(Bruch(1,4))
+
+def test_multipliziere() -> None:
+    assert Bruch(1,2) == Bruch(3,4).multipliziere(Bruch(2,3))
+
+def test_dividiere() -> None:
+    assert Bruch(1,2) == Bruch(3,4).dividiere(Bruch(3,2))

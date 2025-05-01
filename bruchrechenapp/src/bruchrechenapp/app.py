3a397e6b9ddea7bb375c5fbc2c07b6fe59f7fef5
Polu Tartakower <phish@quantentunnel.de>
2025-05-01 18:59:46 +0200

    Divider

--- bruchrechenapp/src/bruchrechenapp/app.py
+++ bruchrechenapp/src/bruchrechenapp/app.py
@@ -9,49 +9,44 @@ from bruchrechenapp.bruch import Bruch
 from bruchrechenapp.operation import Operator
 
 class BruchrechenApp(toga.App):
+
+    def __init__(self, formal_name: str | None = None, app_id: str | None = None, app_name: str | None = None):
+        super().__init__(formal_name, app_id, app_name)
+        self.zaehler_1 = createTextInput()
+        self.nenner_1 = createTextInput()
+        self.auswahl = toga.Selection(items=['+','-','*',':'], style=Pack(font_size=16))
+        self.zaehler_2 = createTextInput()
+        self.nenner_2 = createTextInput()
+        self.zaehler_res = createTextInput()
+        self.nenner_res = createTextInput()
+
     def startup(self):
         
         main_box = toga.Box(style=Pack(direction='column', alignment='center'))
 
         row_box = toga.Box(style=Pack(direction='row', alignment='center', padding=10))
 
-        zaehler_1 = createTextInput()
-        nenner_1 = createTextInput()
-        box_1 = toga.Box(style=Pack(direction='column', padding=10))
-        box_1.add(zaehler_1)
-        box_1.add(nenner_1)
+        box_1 = toga.Box(style=Pack(direction='column', alignment='center', padding=10, flex=0.2))
+        box_1.add(self.zaehler_1)
+        box_1.add(toga.Divider(direction=0, style=Pack(height=5, background_color='black')))
+        box_1.add(self.nenner_1)
+        
+        box_2 = toga.Box(style=Pack(direction='column',  alignment='center',padding=10))
+        box_2.add(self.auswahl)
 
-        auswahl = toga.Selection(items=['+','-','*',':'], style=Pack(font_size=16))
-        box_2 = toga.Box(style=Pack(direction='column', padding=10))
-        box_2.add(auswahl)
+        box_3 = toga.Box(style=Pack(direction='column',  alignment='center',padding=10, flex=0.2))
+        box_3.add(self.zaehler_2)
+        box_3.add(toga.Divider(direction=0, style=Pack(height=5, background_color='black')))
+        box_3.add(self.nenner_2)
 
-        zaehler_2 = createTextInput()
-        nenner_2 = createTextInput()
-        box_3 = toga.Box(style=Pack(direction='column', padding=10))
-        box_3.add(zaehler_2)
-        box_3.add(nenner_2)
-
-        def berechne(rechen_button):
-            try:
-                op = Operator(auswahl.value)
-                b1 = Bruch(int(zaehler_1.value), int(nenner_1.value))
-                b2 = Bruch(int(zaehler_2.value), int(nenner_2.value))
-                b = op.berechne(b1, b2)
-                zaehler_res.value = b.zaehler
-                nenner_res.value = b.nenner
-            except ValueError:
-                zaehler_res.value = '?'
-                nenner_res.value = '?'
-
-        rechnen_button = toga.Button("=", on_press=berechne, style=Pack(padding=5, font_size=16))
-        box_4 = toga.Box(style=Pack(direction='column', padding=10))
+        rechnen_button = toga.Button(text="=", on_press=self.berechne, style=Pack(padding=5, font_size=16))
+        box_4 = toga.Box(style=Pack(direction='column',  alignment='center',padding=10))
         box_4.add(rechnen_button)
 
-        zaehler_res = createTextInput()
-        nenner_res = createTextInput()
-        box_5 = toga.Box(style=Pack(direction='column', padding=10))
-        box_5.add(zaehler_res)
-        box_5.add(nenner_res)
+        box_5 = toga.Box(style=Pack(direction='column',  alignment='center', padding=10, flex=0.2))
+        box_5.add(self.zaehler_res)
+        box_5.add(toga.Divider(direction=0, style=Pack(height=5, background_color='black')))
+        box_5.add(self.nenner_res)
 
         row_box.add(box_1)
         row_box.add(box_2)
@@ -59,15 +54,7 @@ class BruchrechenApp(toga.App):
         row_box.add(box_4)
         row_box.add(box_5)
 
-        def loesche(widget):
-            zaehler_1.value = ''
-            nenner_1.value = ''
-            zaehler_2.value = ''
-            nenner_2.value = ''
-            zaehler_res.value = ''
-            nenner_res.value = ''
-
-        clear_button = toga.Button("Neue Aufgabe", on_press=loesche, style=Pack(width=180, padding=5, font_size=16))
+        clear_button = toga.Button(text="Neue Aufgabe", on_press=self.loesche, style=Pack(width=180, padding=5, font_size=16))
 
         main_box.add(row_box)
         main_box.add(clear_button)
@@ -76,8 +63,28 @@ class BruchrechenApp(toga.App):
         self.main_window.content = main_box
         self.main_window.show()
 
+    def berechne(self, button: toga.Button):
+        try:
+            op = Operator(self.auswahl.value)
+            b1 = Bruch(int(self.zaehler_1.value), int(self.nenner_1.value))
+            b2 = Bruch(int(self.zaehler_2.value), int(self.nenner_2.value))
+            b = op.berechne(b1, b2)
+            self.zaehler_res.value = b.zaehler
+            self.nenner_res.value = b.nenner
+        except ValueError:
+            self.zaehler_res.value = '?'
+            self.nenner_res.value = '?'
+
+    def loesche(self, button: toga.Button):
+        self.zaehler_1.value = ''
+        self.nenner_1.value = ''
+        self.zaehler_2.value = ''
+        self.nenner_2.value = ''
+        self.zaehler_res.value = ''
+        self.nenner_res.value = ''
+
 def createTextInput() -> toga.TextInput:
-    return toga.TextInput(style=Pack(width=60, text_align="center", padding=5, font_size=16))
+    return toga.TextInput(style=Pack(width=60, text_align="center", padding=5, font_size=16, background_color='yellow'))
 
 def main():
     return BruchrechenApp()
